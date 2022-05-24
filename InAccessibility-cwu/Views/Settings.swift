6cccb9dc4dd0fc403bbbb56c21b7c11781be6282
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2022-05-24 12:13:03 -0400

    Made hints much more descriptive about what the colors they're replacing indicate.

--- InAccessibility-cwu/Views/Settings.swift
+++ InAccessibility-cwu/Views/Settings.swift
@@ -14,23 +14,22 @@ struct Settings: View {
         Form {
             Section {
                 Picker("Choose colors", selection: $colorBlindModeFound) {
-                    ForEach(ColorBlindMode.allCases, id:\.self) { oneColor in
+                    ForEach(ColorBlindMode.allCases, id:\.self) { oneColorMode in
                         VStack(alignment: .leading) {
-                            Text("\(oneColor.rawValue)")
+                            Text("\(oneColorMode.rawValue)")
                             HStack {
                                 Rectangle()
-                                    .foregroundColor(colorsToUse(modeToUse: oneColor).greenColor)
+                                    .foregroundColor(colorsToUse(modeToUse: oneColorMode).greenColor)
                                     .frame(width: UIConstants.ColorChoiceSize, height: UIConstants.ColorChoiceSize)
 
                                 Rectangle()
-                                    .foregroundColor(colorsToUse(modeToUse: oneColor).redColor)
+                                    .foregroundColor(colorsToUse(modeToUse: oneColorMode).redColor)
                                     .frame(width: UIConstants.ColorChoiceSize, height: UIConstants.ColorChoiceSize)
                             }
                         }
                         .accessibilityElement(children: .combine)
                         .accessibilityLabel(Text("Choose colors"))
-                        // props to Kris Slazinski for informing me you can use ".description" to get a color description.
-                        .accessibilityHint(Text("\(colorsToUse(modeToUse: oneColor).greenColor.description) will be used for the \"green\" color and \(colorsToUse(modeToUse: oneColor).redColor.description) will be used for the \"red\" color."))
+                        .accessibilityHint(Text("\(makeAccessibilityHints(colorMode: oneColorMode))"))
                     }
                 }
             } header: {
@@ -39,6 +38,27 @@ struct Settings: View {
                Text("Choose what colors to use instead of a green/red combination.")
             }
         }
+    }
+    
+    // props to Kris Slazinski for informing me you can use ".description" to get a color description.
+    func makeAccessibilityHints(colorMode : ColorBlindMode) -> String {
+        let greenPart : String
+        let redPart : String
+        let foundColors = colorsToUse(modeToUse: colorMode)
+        
+        if Color.green == foundColors.greenColor {
+            greenPart = "\(foundColors.greenColor.description) will be used when the stock goes up. "
+        } else {
+            greenPart = "\(foundColors.greenColor.description) will be used instead of \(Color.green.description) when the stock goes up. "
+        }
+        
+        if Color.red == foundColors.redColor {
+            redPart = "\(foundColors.redColor.description) will be used when the stock goes down."
+        } else {
+            redPart = "\(foundColors.redColor.description) will be used instead of \(Color.red.description) when the stock goes down."
+        }
+        
+        return greenPart + redPart
     }
 }
 
